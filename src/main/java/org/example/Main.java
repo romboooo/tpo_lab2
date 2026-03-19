677044e05fc01797b66403a8f38935a12bab47d5
romboooo <412902@niuitmo.ru>
2026-03-19 20:27:07 +0300

    feat: func system solve logic

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,11 @@
 package org.example;
 
-import org.example.core.LogarithmicFunction;
-import org.example.core.TrigonometricFunction;
 import org.example.logarithms.Ln;
-import org.example.logarithms.Log;
+import org.example.system.FunctionSystem;
 import org.example.trigonometry.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\n=== DEBUG NORMALIZATION ===");
-        Ln debugLn = new Ln(1e-6, 100);
-
-// x = 8: должно нормализоваться до 1.0, k = 3
-        System.out.printf("ln(8) = %.6f (ожидаемо: %.6f)%n",
-                debugLn.solve(8.0), Math.log(8));
-
-// x = 0.25: должно нормализоваться до 1.0, k = -2
-        System.out.printf("ln(0.25) = %.6f (ожидаемо: %.6f)%n",
-                debugLn.solve(0.25), Math.log(0.25));
-
-// Проверка константы
-        System.out.printf("LN_2 (внутренняя) ≈ %.15f (эталон: %.15f)%n",
-                debugLn.solve(2.0), Math.log(2));
+        System.out.println("ssdsdsds");
     }
 }
---
+++ src/main/java/org/example/system/FunctionSystem.java
@@ -0,0 +1,51 @@
+package org.example.system;
+
+import org.example.logarithms.*;
+import org.example.trigonometry.*;
+
+import static java.lang.Math.*;
+
+public class FunctionSystem {
+    private final Csc csc;
+    private final Sin sin;
+    private final Sec sec;
+    private final Cot cot;
+    private final Ln ln;
+    private final Log log2;
+    private final Log log5;
+    private final Log log10;
+
+    public FunctionSystem(double epsilon, int maxIterations) {
+        this.sin = new Sin(epsilon, maxIterations);
+        this.sec = new Sec(epsilon, maxIterations);
+        this.cot = new Cot(epsilon, maxIterations);
+        this.csc = new Csc(epsilon, maxIterations);
+
+        this.ln = new Ln(epsilon, maxIterations);
+        this.log2 = new Log(2.0, epsilon, maxIterations);
+        this.log5 = new Log(5.0, epsilon, maxIterations);
+        this.log10 = new Log(10.0, epsilon, maxIterations);
+    }
+
+    public double solve(double x) {
+        return (x <= 0) ? solveFirstBranch(x) : solveSecondBranch(x);
+    }
+
+    public double solveFirstBranch(double x) {
+        if (!sin.isDefined(x) || !csc.isDefined(x) ||
+                !sec.isDefined(x) || !cot.isDefined(x)) {
+            return Double.NaN;
+        }
+
+        double firstTerm = pow(pow((csc.solve(x) / sin.solve(x)), 2) * sec.solve(x), 3);
+        double secondTerm = cot.solve(x) + pow(sec.solve(x), 6);
+        return firstTerm - secondTerm;
+    }
+
+    public double solveSecondBranch(double x) {
+        return ((pow(ln.solve(x), 2) * log2.solve(x)
+                + log2.solve(x))
+                + log10.solve(x))
+                - log5.solve(x);
+    }
+}
